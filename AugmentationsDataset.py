deb08cffa83746401999a275d581fafc8b496a39
Tom Bekor <tom.bekor@gmail.com>
2022-05-24 15:53:21 +0300

    gets and aplies augmentations list

--- AugmentationsDataset.py
+++ AugmentationsDataset.py
@@ -5,14 +5,15 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
-
-
 class AugmentationsDataset(Dataset):
-    def __init__(self, dataset_glob_path, source_transform, target_transform):
+    def __init__(self, dataset_glob_path,
+                 source_transform, target_transform,
+                 augmentations):
         super().__init__()
         self.dataset_image_paths = np.array(glob.glob(dataset_glob_path))
         self.source_transform = source_transform
         self.target_transform = target_transform
+        self.augmentations = augmentations
 
     def __len__(self):
         return len(self.dataset_image_paths)
@@ -23,7 +24,10 @@ class AugmentationsDataset(Dataset):
 
         image_path = self.dataset_image_paths[idx]
         image = Image.open(image_path)
-        source_image = self.source_transform(image) # input image
-        target_image = self.target_transform(image) # augmented image
+        source_image = self.source_transform(image) # tensored image
+        target_image = self.target_transform(image) # tensored image
+
+        for augment in self.augmentations:
+            target_image = augment(target_image)        
 
         return source_image, target_image
--- utils.py
+++ utils.py
@@ -1,7 +1,44 @@
+import imp
 import numpy as np
-import torch
 import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+import kornia as K
 
 
 def plot_tensor(t):
     plt.imshow(np.array(t.permute(1,2,0)))
+
+def kornia_rotation(img, degrees):
+    # unsqueeze img
+    img = img.unsqueeze(0)
+
+    # create transformation (rotation)
+    alpha: float = 45.0  # in degrees
+    angle: torch.tensor = torch.ones(1) * alpha
+
+    # define the rotation center
+    center: torch.tensor = torch.ones(1, 2)
+    center[..., 0] = img.shape[3] / 2  # x
+    center[..., 1] = img.shape[2] / 2  # y
+
+    # define the scale factor
+    scale: torch.tensor = torch.ones(1, 2)
+
+    # compute the transformation matrix
+    M: torch.tensor = K.geometry.get_rotation_matrix2d(center, angle, scale)
+
+    _, _, h, w = img.shape
+    img_warped: torch.tensor = K.geometry.warp_affine(img, M, dsize=(h, w))
+
+    return img_warped.squeeze(0)
+
+def show_sample(input, size: tuple = None):
+    images = torch.stack(input, dim=0)
+    out = torchvision.utils.make_grid(images, nrow=4, padding=2)
+    out_np: np.ndarray = K.utils.tensor_to_image(out)
+    plt.figure(figsize=size)
+    plt.imshow(out_np)
+    plt.axis('off')
+    plt.show()
