5dea728222ee620ef2ccd75592540c2ae32a074f
Tom Bekor <tom.bekor@gmail.com>
2022-05-25 18:43:41 +0300

    create grid of learning process

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import imp
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 import torch
 import torchvision
@@ -36,9 +37,32 @@ def kornia_rotation(img, degrees):
 
 def show_sample(input, size: tuple = None):
     images = torch.stack(input, dim=0)
-    out = torchvision.utils.make_grid(images, nrow=4, padding=2)
+    out = torchvision.utils.make_grid(images, nrow=4, padding=5, pad_value=1)
     out_np: np.ndarray = K.utils.tensor_to_image(out)
     plt.figure(figsize=size)
     plt.imshow(out_np)
     plt.axis('off')
     plt.show()
+
+
+def learning_grid(img_dict: dict, save=None):
+    fig = plt.figure(figsize=(15,20))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                    nrows_ncols=(1, 3),  # creates 2x2 grid of axes
+                    axes_pad=(2,10),  # pad between axes in inch.
+                    )
+
+    for ax, im_name in zip(grid, img_dict.keys()):
+        # Iterating over the grid returns the Axes.
+        ax.set_title(im_name, fontsize=20)
+        im = np.array(img_dict[im_name].permute(1,2,0).detach())
+        ax.imshow(im)
+
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    
+    if save:
+        plt.savefig(save)
+        plt.close()
+    else:
+        plt.show()
