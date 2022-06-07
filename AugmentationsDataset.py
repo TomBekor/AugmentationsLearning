e24703ffa1379979fdd36108de6770d4ee3dfccf
Tom Bekor <tom.bekor@gmail.com>
2022-06-07 18:35:53 +0300

    some images in Tiny ImageNet are in wrong format

--- AugmentationsDataset.py
+++ AugmentationsDataset.py
@@ -22,11 +22,15 @@ class AugmentationsDataset(Dataset):
 
         image_path = self.dataset_image_paths[idx]
         image = Image.open(image_path)
+        if image.mode != 'RGB':
+            # print(image.mode)
+            return self.__getitem__((idx + 1) % len(self))
         source_image = self.source_transform(image) # tensored image
         target_image = self.target_transform(image).squeeze(0) # tensored image
         source_image.requires_grad_(True)
         target_image.requires_grad_(True)
 
+        # broken images in the original dataset
         if source_image.shape != (3,64,64) or target_image.shape != (3,64,64):
             return self.__getitem__((idx + 1) % len(self))
             
