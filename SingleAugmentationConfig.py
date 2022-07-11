4a25fae5cf1f0d7ced440866d7829f634434c588
Tom Bekor <tom.bekor@gmail.com>
2022-07-11 20:46:09 +0300

    configurations added

--- SingleAugmentationConfig.py
+++ SingleAugmentationConfig.py
@@ -4,6 +4,10 @@ import torch.optim as optim
 import kornia as K
 from kAugmentations.kContrast import *
 from kAugmentations.kTranslateX import *
+from kAugmentations.kRotation import *
+from kAugmentations.kBrightness import *
+
+
 
 
 class SingleAugmentationConfig:
@@ -50,14 +54,14 @@ contrast_config = SingleAugmentationConfig(
     main_parameter_name = 'contrast_factor',
     aug_bounds = (0.0, None),
     param_linspace = np.linspace(0.0,10.,400),
-    target_param_val = 3.,
+    target_param_val = 2.,
     target_aug_constructor = K.enhance.AdjustContrast,
-    target_aug_constructor_args = {'contrast_factor': 3.},
+    target_aug_constructor_args = {'contrast_factor': 2.},
     kAugmentation = kContrast,
     init_param_val = 1.,
     optimizer_constructor = optim.SGD,
     optimizer_constructor_args = {
-        'lr': 0.1,
+        'lr': .1,
         'momentum': 0.9,
     },
     scheduler_constructor = optim.lr_scheduler.CosineAnnealingLR,
@@ -72,20 +76,14 @@ contrast_config = SingleAugmentationConfig(
     early_stopping = 250,
 )
 
-
-
-
-
-
-
 translateX_config = SingleAugmentationConfig(
     augmentation_name = 'translateX',
     main_parameter_name = 'translation',
     aug_bounds = (None, None),
     param_linspace = np.linspace(-100,100,400),
-    target_param_val = 20.0,
+    target_param_val = 10.0,
     target_aug_constructor = K.geometry.transform.Translate,
-    target_aug_constructor_args = {'translation': torch.Tensor([[20.0, 0]]),},
+    target_aug_constructor_args = {'translation': torch.Tensor([[10.0, 0]]),},
     kAugmentation = kTranslateX,
     init_param_val = 0.0,
     optimizer_constructor = optim.SGD,
@@ -95,7 +93,7 @@ translateX_config = SingleAugmentationConfig(
     },
     scheduler_constructor = optim.lr_scheduler.CosineAnnealingLR,
     scheduler_constructor_args = {
-        'T_max':40,
+        'T_max':100,
         'eta_min':10,
         'verbose':True,
     },
@@ -103,4 +101,60 @@ translateX_config = SingleAugmentationConfig(
     scheduler_freq = 10,
     use_scheduler=True,
     early_stopping = 250,
+)
+
+
+rotation_config = SingleAugmentationConfig(
+    augmentation_name = 'rotation',
+    main_parameter_name = 'angle',
+    aug_bounds = (-360., 360.),
+    param_linspace = np.linspace(0., 360.,200),
+    target_param_val = 30.,
+    target_aug_constructor = K.geometry.transform.Rotate,
+    target_aug_constructor_args = {'angle': torch.Tensor([30.])},
+    kAugmentation = kRotation,
+    init_param_val = 0.,
+    optimizer_constructor = optim.SGD,
+    optimizer_constructor_args = {
+        'lr': 100,
+        'momentum': 0.99,
+    },
+    scheduler_constructor = optim.lr_scheduler.CosineAnnealingLR,
+    scheduler_constructor_args = {
+        'T_max':100,
+        'eta_min':10,
+        'verbose':True,
+    },
+    scheduler_warmup = 100,
+    scheduler_freq = 10,
+    use_scheduler=True,
+    early_stopping = 500,
+)
+
+
+brightness_config = SingleAugmentationConfig(
+    augmentation_name = 'brightness',
+    main_parameter_name = 'brightness_factor',
+    aug_bounds = (0.0, 1.0),
+    param_linspace = np.linspace(0., 1.,200),
+    target_param_val = .25,
+    target_aug_constructor = K.enhance.AdjustBrightness,
+    target_aug_constructor_args = {'brightness_factor': .25},
+    kAugmentation = kBrightness,
+    init_param_val = 0.0,
+    optimizer_constructor = optim.SGD,
+    optimizer_constructor_args = {
+        'lr': .01,
+        'momentum': 0.9,
+    },
+    use_scheduler=True,
+    scheduler_constructor = optim.lr_scheduler.CosineAnnealingLR,
+    scheduler_constructor_args = {
+        'T_max':100,
+        'eta_min':0.001,
+        'verbose':True,
+    },
+    scheduler_warmup = 100,
+    scheduler_freq = 10,
+    early_stopping = 500,
 )
